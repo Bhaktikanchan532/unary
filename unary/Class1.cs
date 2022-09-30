84e76d7f0895e800493f6da31f03f9e08c19f88d
Bhaktikanchan532 <kanchanbhakti@gmail.com>
2022-09-30 15:58:19 +0530

    example

---
+++ unary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unary
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+       {
+                   int a = 10;
+                    int b = ++a;
+                    int c = --b * ++a;
+                    int d = ++c * ++b + --a;
+                    Console.WriteLine(a);
+                   Console.WriteLine(b);
+                   Console.WriteLine(c);
+                   Console.WriteLine(d);
+              }
+    }
+}
--- unary/Program.cs
+++ unary/Program.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace unary
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace unary
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-            int a = 10;
-            int b = ++a + a++;
-            int c = ++b * a++;
-            int d = ++c * ++b - ++a;
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-        }
-    }
-}
+//            int a = 10;
+//            int b = ++a + a++;
+//            int c = ++b * a++;
+//            int d = ++c * ++b - ++a;
+//            Console.WriteLine(a);
+//            Console.WriteLine(b);
+//            Console.WriteLine(c);
+//            Console.WriteLine(d);
+//        }
+//    }
+//}
