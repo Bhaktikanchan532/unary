ea8a0482f7ea57c8b8b4a917566f87646758bf0d
Bhaktikanchan532 <kanchanbhakti@gmail.com>
2022-09-30 16:47:40 +0530

    unaryexmple

--- unary/Class1.cs
+++ unary/Class1.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace unary
-{
-    internal class Class1
-    {
-        static void Main(string[] args)
-       {
-                   int a = 10;
-                    int b = ++a;
-                    int c = --b * ++a;
-                    int d = ++c * ++b + --a;
-                    Console.WriteLine(a);
-                   Console.WriteLine(b);
-                   Console.WriteLine(c);
-                   Console.WriteLine(d);
-              }
-    }
-}
+//namespace unary
+//{
+//    internal class Class1
+//    {
+//        static void Main(string[] args)
+//       {
+//                   int a = 10;
+//                    int b = ++a;
+//                    int c = --b * ++a;
+//                    int d = ++c * ++b + --a;
+//                    Console.WriteLine(a);
+//                   Console.WriteLine(b);
+//                   Console.WriteLine(c);
+//                   Console.WriteLine(d);
+//              }
+//    }
+//}
